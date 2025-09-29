b66c05074b2a181e39bdb5096ddd47cd682cc04b
MoNRaSpGit <ju4nrsuarez@gmail.com>
2025-09-28 21:50:08 -0300

    ddd

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -23,17 +23,7 @@ function normalizeText(text) {
 /**
  * Obtener todos los productos
  */
-export async function getProducts(req, res) {
-  try {
-    const [rows] = await pool.query(
-      "SELECT id, name, price, barcode, description FROM products"
-    );
-    res.json(rows);
-  } catch (err) {
-    console.error("❌ Error consultando productos:", err);
-    res.status(500).json({ error: "Error al obtener productos" });
-  }
-}
+getProducts
 
 /**
  * Actualizar producto (nombre y precio)
